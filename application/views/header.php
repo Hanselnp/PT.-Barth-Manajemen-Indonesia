efa78a1c4f5cbdfa63c7af5fc1951cd8f2993fc8
hanselnp <www.hanselnp99@gmail.com>
2019-11-23 21:33:45 +0700

    Logo and image update

---
+++ application/views/header.php
@@ -0,0 +1,66 @@
+      <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="<?php echo base_url() ?>/css/main.css">
+<link rel="icon favicon" href="<?php echo base_url() ?>/images/logoBMI.png">
+
+<!-- Javascript -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1 /jquery.min.js"></script>
+<script
+src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
+crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>/js/font-awesome/all.js" charset="utf-8"></script>
+<script src="<?php echo base_url() ?>/js/sly/sly.min.js" charset="utf-8"></script>
+<script src="<?php echo base_url() ?>/js/main.js" charset="utf-8"></script>
+<div class="container-fluid p-0">
+  <div class="row no-gutters">
+    <div class="col-12">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand ml-md-2 ml-lg-3" href="#">
+          <img src="<?php echo base_url() ?>/images/logoBMI.png" width="40" height="40" class="d-inline-block align-middle" alt="">
+          <span class="ml-1 navbar-brand-text d-inline-block align-middle">
+            <text class="top-text">PT. Barth Management</text>
+            <br>
+            <text class="bottom-text">Indonesia</text></span>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto mr-lg-3">
+            <li class="nav-item active">
+              <a class="nav-link" href="<?php echo base_url();?>home/index">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url();?>home/about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="<?php echo base_url();?>home/service">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Our Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <!-- <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li> -->
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</div>
--- application/views/home.php
+++ application/views/home.php
@@ -1,81 +1,16 @@
 <html lang="en">
 <title>PT. Barth Management Indonesia</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo base_url() ?>/css/main.css">
-  <link rel="icon favicon" href="<?php echo base_url() ?>/images/logo.png">
-
-  <!-- Javascript -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script
-  src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
-  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
-  crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <script src="<?php echo base_url() ?>/js/font-awesome/all.js" charset="utf-8"></script>
-  <script src="<?php echo base_url() ?>/js/sly/sly.min.js" charset="utf-8"></script>
-  <script src="<?php echo base_url() ?>/js/main.js" charset="utf-8"></script>
 </head>
 <body>
-<div class="container-fluid p-0">
-  <div class="row no-gutters">
-    <div class="col-12">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand ml-md-2 ml-lg-3" href="#">
-          <img src="<?php echo base_url() ?>/images/logo.png" width="40" height="40" class="d-inline-block align-middle" alt="">
-          <span class="ml-1 navbar-brand-text d-inline-block align-middle">
-            <text class="top-text">PT. Barth Management</text>
-            <br>
-            <text class="bottom-text">Indonesia</text></span>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+  <?php include 'header.php'; ?>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto mr-lg-3">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Services</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Our Team</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contact Us</a>
-            </li>
-            <!-- <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li> -->
-          </ul>
-        </div>
-      </nav>
-    </div>
-  </div>
-</div>
-
-<div class="jumbotron text-left" style="background-image: url('<?php echo base_url() ?>/images/banner_1920x1200.jpg')">
+<div class="jumbotron text-left" style="background-color: white; ">
   <div class="overlay"></div>
   <div class="jumbotron-inner">
     <h1>
-      This is the company slogan. <br> Slogan two and three here.
+      Menyediakan Solusi Untuk <br> Permasalahan Manajemen Anda.
     </h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <p>Kami merupakan perushaan yang bergerak pada jasa manajemen lainnya</p>
     <a href="#" class="a-button">Learn More</a>
   </div>
 </div>
